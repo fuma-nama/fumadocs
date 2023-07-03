0ad92f403597a63f5e02852f0fc5077b53667a49
SonMooSans <god63820869@gmail.com>
2023-07-03 19:42:55 +0800

    Reduce bundle size

--- packages/next-docs/tsup.config.ts
+++ packages/next-docs/tsup.config.ts
@@ -3,7 +3,12 @@ import tsconfig from "./tsconfig.json";
 
 export default defineConfig({
     name: "next-docs",
-    entry: ["src/api/index.ts", "src/components/index.ts", "src/lib/index.ts"],
+    entry: [
+        "src/api/index.ts",
+        "src/components/index.ts",
+        "src/lib/index.ts",
+        "src/search/index.ts",
+    ],
     format: "esm",
     dts: true,
     target: tsconfig.compilerOptions.target as "es2016",
