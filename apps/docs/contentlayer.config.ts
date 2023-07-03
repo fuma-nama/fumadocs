02abfe01b8c19a2f6f3beb4cd1534c30f23a164e
SonMooSans <god63820869@gmail.com>
2023-07-03 19:41:40 +0800

    Remove unnecessary parts

--- apps/docs/contentlayer.config.ts
+++ apps/docs/contentlayer.config.ts
@@ -1,8 +1,4 @@
-import {
-    defineDocumentType,
-    defineNestedType,
-    makeSource,
-} from "contentlayer/source-files";
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
 import rehypePrettycode from "rehype-pretty-code";
 import remarkGfm from "remark-gfm";
 import rehypeSlug from "rehype-slug";
@@ -66,79 +62,9 @@ const Meta = defineDocumentType(() => ({
     },
 }));
 
-const Blog = defineDocumentType(() => ({
-    name: "Blog",
-    filePathPattern: "blog/*.mdx",
-    contentType: "mdx",
-    fields: {
-        title: {
-            type: "string",
-            description: "The title of the document",
-            required: true,
-        },
-        description: {
-            type: "string",
-            description: "The description of the document",
-            required: false,
-        },
-        date: {
-            type: "date",
-            description: "The release Date of the Post",
-            required: true,
-        },
-        image: {
-            type: "string",
-            description: "The image url (can be relative)",
-            required: true,
-        },
-        author: {
-            type: "string",
-            description: "The name of the author",
-            required: true,
-        },
-    },
-    computedFields: {
-        url: {
-            type: "string",
-            resolve: (blog) => {
-                return "/" + blog._raw.flattenedPath;
-            },
-        },
-        slug: {
-            type: "string",
-            resolve: (blog) => {
-                return blog._raw.flattenedPath.split("/").slice(1).join("/");
-            },
-        },
-    },
-}));
-
-const Author = defineNestedType(() => ({
-    name: "Author",
-    fields: {
-        name: { type: "string", required: true },
-        title: { type: "string", required: true },
-        icon: { type: "string", required: true },
-    },
-}));
-
-const BlogMeta = defineDocumentType(() => ({
-    name: "BlogMeta",
-    filePathPattern: "blog/meta.json",
-    contentType: "data",
-    fields: {
-        authors: {
-            type: "list",
-            of: Author,
-            required: true,
-        },
-    },
-    isSingleton: true,
-}));
-
 export default makeSource({
     contentDirPath: "content",
-    documentTypes: [Docs, Meta, Blog, BlogMeta],
+    documentTypes: [Docs, Meta],
     mdx: {
         rehypePlugins: [
             [
