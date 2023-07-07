5e244c516919ad0827714b069fa1845df207b661
SonMooSans <god63820869@gmail.com>
2023-07-07 13:15:39 +0800

    Fix missing key warnings

--- apps/docs/components/mdx/heading.tsx
+++ apps/docs/components/mdx/heading.tsx
@@ -1,36 +1,29 @@
 import clsx from "clsx";
 import { LinkIcon } from "lucide-react";
-import { ComponentPropsWithoutRef, createElement } from "react";
+import { ComponentPropsWithoutRef } from "react";
 
 type Types = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
-
-type InternalHeadingProps<T extends Types> = {
-    as: T;
+type HeadingProps<T extends Types> = Omit<ComponentPropsWithoutRef<T>, "as"> & {
+    as?: T;
 };
 
-export function Heading<T extends Types>({
+export function Heading<T extends Types = "h1">({
     id,
     as,
     ...props
-}: Omit<ComponentPropsWithoutRef<T>, keyof InternalHeadingProps<T>> &
-    InternalHeadingProps<T>) {
-    return createElement(
-        as,
-        {
-            ...props,
-            className: clsx("group", props.className),
-        },
-        [
-            <>
-                <span id={id} className="absolute -mt-20" />
-                {props.children}
-                <a
-                    href={`#${id}`}
-                    className="opacity-0 group-hover:opacity-100 inline-block ml-2 text-muted-foreground"
-                >
-                    <LinkIcon className="w-4 h-4" />
-                </a>
-            </>,
-        ]
+}: HeadingProps<T>) {
+    const As = as ?? "h1";
+
+    return (
+        <As {...props} className={clsx("group", props.className)}>
+            <span id={id} className="absolute -mt-20" />
+            {props.children}
+            <a
+                href={`#${id}`}
+                className="opacity-0 group-hover:opacity-100 inline-block ml-2 text-muted-foreground"
+            >
+                <LinkIcon className="w-4 h-4" />
+            </a>
+        </As>
     );
 }
