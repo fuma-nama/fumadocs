aed1083886c1fcdceae459b99a37f66df3a8b9a0
SonMooSans <god63820869@gmail.com>
2023-07-27 21:31:03 +0800

    UI: Support custom TOC content

--- packages/next-docs-ui/src/page.tsx
+++ packages/next-docs-ui/src/page.tsx
@@ -4,24 +4,28 @@ import { Breadcrumb } from "@/components/breadcrumb";
 import { TOC } from "@/components/toc";
 import { ReactNode } from "react";
 
-export function DocsPage({
-    toc,
-    tree,
-    children,
-}: {
+export type DocsPageProps = {
     toc: TableOfContents;
+
+    /**
+     * Custom content in TOC container
+     */
+    tocContent?: ReactNode;
     tree: TreeNode[];
     children: ReactNode;
-}) {
+};
+
+export function DocsPage(props: DocsPageProps) {
     return (
         <>
             <article className="nd-flex nd-flex-col nd-gap-6 nd-py-8 nd-mb-20 md:nd-py-16">
-                <Breadcrumb tree={tree} />
-                {children}
+                <Breadcrumb tree={props.tree} />
+                {props.children}
             </article>
             <div className="nd-relative max-xl:nd-hidden">
-                <div className="nd-sticky nd-flex nd-top-14 nd-py-16 nd-max-h-[calc(100vh-3.5rem)]">
-                    <TOC items={toc} />
+                <div className="nd-sticky nd-flex nd-flex-col nd-top-14 nd-py-16 nd-max-h-[calc(100vh-3.5rem)]">
+                    <TOC items={props.toc} />
+                    {props.tocContent}
                 </div>
             </div>
         </>
