4fa45c0f928249bb63e3cce1f350d3b5f4e4c3b7
SonMooSans <god63820869@gmail.com>
2023-07-28 20:53:53 +0800

    Docs: Update configuration

---
+++ .changeset/perfect-cups-vanish.md
@@ -0,0 +1,5 @@
+---
+"next-docs-zeta": minor
+---
+
+Add support for dynamic hrefs and relative paths to `<SafeLink />` component
--- apps/docs/utils/page-tree.ts
+++ apps/docs/utils/page-tree.ts
@@ -1,15 +1,13 @@
 import { allDocs, allMeta } from "contentlayer/generated";
-import {
-    buildPageTreeWithContext,
-    preloadContext,
-} from "next-docs-zeta/contentlayer";
+import { buildPageTree, loadContext } from "next-docs-zeta/contentlayer";
 import type { TreeNode } from "next-docs-zeta/server";
 
-const ctx = preloadContext(allMeta, allDocs);
-export const uiTree = buildPageTreeWithContext(ctx, {
+const ctx = loadContext(allMeta, allDocs);
+
+export const uiTree = buildPageTree(ctx, {
     root: "docs/ui",
 });
-export const headlessTree = buildPageTreeWithContext(ctx, {
+export const headlessTree = buildPageTree(ctx, {
     root: "docs/headless",
 });
 
