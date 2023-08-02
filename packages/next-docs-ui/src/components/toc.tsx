1b626c942bd0e17baa9d11d10cb5d5df3bce98a0
SonMooSans <god63820869@gmail.com>
2023-08-02 15:30:20 +0800

    UI: Add outline to TOC items

--- packages/next-docs-ui/src/components/toc.tsx
+++ packages/next-docs-ui/src/components/toc.tsx
@@ -1,37 +1,40 @@
+import { cn } from '@/utils/cn'
 import type { TOCItemType } from 'next-docs-zeta/server'
 import * as Primitive from 'next-docs-zeta/toc'
 
 export function TOC({ items }: { items: TOCItemType[] }) {
   return (
-    <Primitive.TOCProvider
-      toc={items}
-      className="nd-flex nd-flex-col nd-gap-1 nd-overflow-hidden nd-flex-1"
-    >
+    <Primitive.TOCProvider toc={items} className="nd-overflow-hidden nd-flex-1">
       {items.length > 0 && (
-        <h3 className="nd-flex nd-flex-row nd-items-center nd-font-medium nd-text-sm nd-mb-2">
-          On This Page
-        </h3>
+        <h3 className="nd-font-medium nd-text-sm nd-mb-4">On This Page</h3>
       )}
-      {items.map((item, i) => (
-        <TOCItem key={i} item={item} />
-      ))}
+      <div className="nd-flex nd-flex-col">
+        {items.map((item, i) => (
+          <TOCItem key={i} item={item} level={0} />
+        ))}
+      </div>
     </Primitive.TOCProvider>
   )
 }
 
-function TOCItem({ item }: { item: TOCItemType }) {
+function TOCItem({ item, level }: { item: TOCItemType; level: number }) {
   return (
-    <div className="nd-flex nd-flex-col nd-gap-1">
+    <>
       <Primitive.TOCItem
         href={item.url}
         item={item}
-        className="nd-text-sm nd-text-muted-foreground nd-transition-colors nd-text-ellipsis nd-overflow-hidden data-[active=true]:nd-font-medium data-[active=true]:nd-text-primary"
+        className={cn(
+          'nd-border-l-2 nd-text-sm nd-py-0.5 nd-text-muted-foreground nd-transition-colors nd-text-ellipsis nd-overflow-hidden data-[active=true]:nd-font-medium data-[active=true]:nd-text-primary data-[active=true]:nd-border-primary',
+          level === 0 && 'nd-pl-3',
+          level === 1 && 'nd-pl-6',
+          level >= 2 && 'nd-pl-9'
+        )}
       >
         {item.title}
       </Primitive.TOCItem>
-      <div className="nd-flex nd-flex-col nd-pl-4">
-        {item.items?.map((item, i) => <TOCItem key={i} item={item} />)}
-      </div>
-    </div>
+      {item.items?.map((item, i) => (
+        <TOCItem key={i} item={item} level={level + 1} />
+      ))}
+    </>
   )
 }
