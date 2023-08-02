1e3de99876b8f19d26844a760b99f4657829da39
SonMooSans <god63820869@gmail.com>
2023-08-02 15:01:55 +0800

    Docs: Change accordion style

--- apps/docs/components/ui/accordion.tsx
+++ apps/docs/components/ui/accordion.tsx
@@ -28,7 +28,7 @@ const AccordionTrigger = React.forwardRef<
       <AccordionPrimitive.Trigger
         ref={ref}
         className={cn(
-          'flex flex-1 items-center justify-between py-4 text-base font-medium [&[data-state=open]>svg]:rotate-180 hover:underline',
+          'flex flex-1 items-center justify-between py-4 text-sm font-medium [&[data-state=open]>svg]:rotate-180',
           className
         )}
         {...props}
