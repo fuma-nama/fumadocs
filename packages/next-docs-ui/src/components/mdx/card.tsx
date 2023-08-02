ce8e7396910e7452c103589e7d014f6f8baea010
SonMooSans <god63820869@gmail.com>
2023-08-02 14:43:37 +0800

    UI: Add icon to card component

--- packages/next-docs-ui/src/components/mdx/card.tsx
+++ packages/next-docs-ui/src/components/mdx/card.tsx
@@ -1,9 +1,10 @@
+import { ExternalLinkIcon } from 'lucide-react'
 import { SafeLink } from 'next-docs-zeta/link'
 import type { ReactNode } from 'react'
 
 export function Cards({ children }: { children: ReactNode }) {
   return (
-    <div className="nd-grid nd-grid-cols-1 md:nd-grid-cols-2 nd-gap-5 nd-not-prose">
+    <div className="nd-grid nd-grid-cols-1 md:nd-grid-cols-2 nd-gap-4 nd-not-prose">
       {children}
     </div>
   )
@@ -11,20 +12,27 @@ export function Cards({ children }: { children: ReactNode }) {
 
 export function Card({
   href,
+  icon = <ExternalLinkIcon />,
   title,
   description
 }: {
   href: string
+  icon?: ReactNode
   title: string
   description: string
 }) {
   return (
     <SafeLink
       href={href}
-      className="nd-flex nd-flex-col nd-gap-2 nd-shadow-lg nd-rounded-xl nd-p-4 nd-border nd-bg-card nd-text-card-foreground nd-transition-colors hover:nd-border-primary hover:nd-shadow-primary/20"
+      className="nd-text-sm nd-rounded-lg nd-p-4 nd-border nd-bg-card nd-text-card-foreground nd-transition-colors hover:nd-bg-accent hover:nd-shadow-sm"
     >
-      <h3 className="nd-font-semibold">{title}</h3>
-      <p className="nd-text-muted-foreground nd-text-sm">{description}</p>
+      <div className="nd-w-fit nd-border nd-p-1 nd-rounded-md nd-bg-background nd-mb-3">
+        <div className="nd-flex nd-justify-center nd-items-center nd-w-8 nd-bg-secondary nd-p-2 nd-h-8 nd-rounded-md nd-text-muted-foreground">
+          {icon}
+        </div>
+      </div>
+      <h3 className="nd-font-medium nd-mb-1">{title}</h3>
+      <p className="nd-text-muted-foreground">{description}</p>
     </SafeLink>
   )
 }
