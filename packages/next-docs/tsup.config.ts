a3b3ab290eddde224fa92ada6af1cf18bce5e80c
SonMooSans <god63820869@gmail.com>
2023-08-04 01:32:20 +0800

    Remove contentlayer from peer deps

--- packages/next-docs/tsup.config.ts
+++ packages/next-docs/tsup.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     'src/{server,breadcrumb,sidebar,toc,search,link,contentlayer,middleware,mdx-plugins}/index.{ts,tsx}',
     'src/contentlayer/configuration.ts'
   ],
+  external: ['contentlayer'],
   format: 'esm',
   dts: true,
   target: tsconfig.compilerOptions.target as 'es2016'
