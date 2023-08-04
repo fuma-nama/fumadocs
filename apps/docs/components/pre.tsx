4ae6cf2c258eb21c6f71cfc43b3c7a96d7df5394
SonMooSans <god63820869@gmail.com>
2023-08-04 14:51:53 +0800

    Docs: Update codeblock styles

--- apps/docs/components/pre.tsx
+++ apps/docs/components/pre.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { cn } from '@/utils/cn'
 import { CheckIcon, CopyIcon } from 'lucide-react'
 import type { ComponentProps } from 'react'
 import { useEffect, useRef, useState } from 'react'
@@ -15,7 +16,7 @@ export function Pre({ title, ...props }: ComponentProps<'pre'>) {
 
   return (
     <div
-      className="relative border rounded-lg bg-secondary/50 text-sm nd-not-prose"
+      className="relative group border rounded-lg bg-secondary/50 text-sm nd-not-prose"
       data-rehype-pretty-code-fragment
     >
       {title && (
@@ -55,15 +56,26 @@ function CopyButton({ onCopy }: { onCopy: () => void }) {
 
   return (
     <button
-      className="absolute top-1 right-1 p-2 border bg-secondary text-secondary-foreground transition-colors rounded-md z-[2] hover:bg-accent"
+      className={cn(
+        'absolute top-2 right-2 p-2 bg-secondary rounded-md transition-all text-secondary-foreground z-[2] opacity-0 group-hover:opacity-100',
+        checked && 'opacity-100'
+      )}
       aria-label="Copy Text"
       onClick={onClick}
     >
-      {checked ? (
-        <CheckIcon className="w-3 h-3" />
-      ) : (
-        <CopyIcon className="w-3 h-3" />
-      )}
+      <CheckIcon
+        className={cn(
+          'absolute w-3.5 h-3.5 transition-transform',
+          !checked && 'scale-0 rotate-90'
+        )}
+      />
+
+      <CopyIcon
+        className={cn(
+          'w-3.5 h-3.5 transition-transform',
+          checked && 'scale-0 rotate-90'
+        )}
+      />
     </button>
   )
 }
