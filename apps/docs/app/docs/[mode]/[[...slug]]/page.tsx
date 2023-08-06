7db10d90df964214611dc277efd6d7fb772bf6c4
SonMooSans <god63820869@gmail.com>
2023-08-06 17:43:07 +0800

    Docs: Split MDX into client components

--- apps/docs/app/docs/[mode]/[[...slug]]/page.tsx
+++ apps/docs/app/docs/[mode]/[[...slug]]/page.tsx
@@ -1,29 +1,12 @@
-import { Pre } from '@/components/pre'
-import { ThemeSwitch } from '@/components/ThemeSwitch'
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger
-} from '@/components/ui/accordion'
 import { getTree } from '@/utils/page-tree'
 import { allDocs } from 'contentlayer/generated'
 import type { Metadata } from 'next'
-import { getMDXComponent } from 'next-contentlayer/hooks'
-import { RollButton } from 'next-docs-ui/components'
-import {
-  Card,
-  Cards,
-  Heading,
-  Image,
-  Link,
-  MDXContent,
-  Table
-} from 'next-docs-ui/mdx'
+import { MDXContent } from 'next-docs-ui/mdx'
 import { DocsPage } from 'next-docs-ui/page'
 import { getPageUrl } from 'next-docs-zeta/contentlayer'
 import { findNeighbour, getTableOfContents } from 'next-docs-zeta/server'
 import { notFound } from 'next/navigation'
+import { DocumentBody } from './body'
 
 type Param = {
   mode: string
@@ -40,7 +23,6 @@ export default async function Page({ params }: { params: Param }) {
   }
 
   const toc = await getTableOfContents(page.body.raw)
-  const MDX = getMDXComponent(page.body.code)
   const url = getPageUrl(page.slug.split('/'), '/docs')
   const neighbours = findNeighbour(tree, url)
 
@@ -68,33 +50,7 @@ export default async function Page({ params }: { params: Param }) {
           </h1>
           <p className="text-muted-foreground sm:text-lg">{page.description}</p>
         </div>
-        <MDX
-          components={{
-            Card: props => <Card {...props} />,
-            Cards: props => <Cards {...props} />,
-            a: props => <Link {...props} />,
-            pre: props => <Pre {...props} />,
-            img: props => <Image {...props} />,
-            h1: props => <Heading as="h1" {...props} />,
-            h2: props => <Heading as="h2" {...props} />,
-            h3: props => <Heading as="h3" {...props} />,
-            h4: props => <Heading as="h4" {...props} />,
-            h5: props => <Heading as="h5" {...props} />,
-            h6: props => <Heading as="h6" {...props} />,
-            table: props => <Table {...props} />,
-            Accordion: props => <Accordion {...props} />,
-            AccordionTrigger: props => <AccordionTrigger {...props} />,
-            AccordionItem: props => <AccordionItem {...props} />,
-            AccordionContent: props => <AccordionContent {...props} />,
-            blockquote: props => (
-              <div className="my-4 rounded-lg border px-3 text-sm shadow-md">
-                {props.children}
-              </div>
-            ),
-            RollButton: props => <RollButton {...props} />,
-            ThemeSwitch: props => <ThemeSwitch {...props} />
-          }}
-        />
+        <DocumentBody code={page.body.code} />
       </MDXContent>
     </DocsPage>
   )
