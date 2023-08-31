5c268dd7bac2fa967454cca6157c68f07f554b3f
SonMooSans <god63820869@gmail.com>
2023-08-31 20:58:47 +0800

    Fix error

--- apps/docs/app/api/og/[mode]/route.ts
+++ apps/docs/app/api/og/[mode]/route.ts
@@ -21,7 +21,7 @@ export async function GET(
       title: title ?? 'Next Docs',
       description: description ?? 'The Documentation Framework',
       isUI: params.mode === 'ui',
-      backgroundImage
+      backgroundImage: await backgroundImage
     }),
     {
       width: 1200,
