cdb274da180178c91696a00364af63c21a488b5d
SonMooSans <god63820869@gmail.com>
2023-09-16 19:06:38 +0800

    CI: Automatically add changeset for create-next-docs-app

---
+++ scripts/on-push.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+
+// Automatically add changeset for create-next-docs-app
+const { writeFileSync, existsSync } = require('fs')
+const path = require('path')
+const dir = path.resolve(process.cwd(), '.changeset')
+if (!existsSync(dir)) throw new Error("Can't find changeset folder")
+
+const content = `---
+'create-next-docs-app': patch
+---
+
+Update Examples`
+
+writeFileSync(path.resolve(dir, 'create-next-docs-app.md'), content)
+
+console.log('Added changeset automatically')
