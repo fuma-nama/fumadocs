5ec7b5f7879be67fe607ce4c811735dea9eae721
Fuma <76240755+fuma-nama@users.noreply.github.com>
2023-09-21 01:36:39 +0800

    Update metadata

--- apps/docs/utils/metadata.ts
+++ apps/docs/utils/metadata.ts
@@ -7,7 +7,7 @@ export function createMetadata(override: Metadata): Metadata {
       title: override.title ?? undefined,
       description: override.description ?? undefined,
       url: 'https://next-docs-zeta.vercel.app',
-      images: '/banner.png',
+      images: '/features.png',
       siteName: 'Next Docs',
       ...override.openGraph
     },
@@ -16,7 +16,7 @@ export function createMetadata(override: Metadata): Metadata {
       creator: '@money_is_shark',
       title: override.title ?? undefined,
       description: override.description ?? undefined,
-      images: '/banner.png',
+      images: '/features.png',
       ...override.twitter
     }
   }
