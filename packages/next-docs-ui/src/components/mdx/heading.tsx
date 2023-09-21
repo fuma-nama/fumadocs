b6889242b73fe85c058e60ee23a5b5b735f9d525
SonMooSans <god63820869@gmail.com>
2023-09-21 21:49:25 +0800

    UI: Change default heading link icon

--- packages/next-docs-ui/src/components/mdx/heading.tsx
+++ packages/next-docs-ui/src/components/mdx/heading.tsx
@@ -1,4 +1,5 @@
-import clsx from 'clsx'
+import { cn } from '@/utils/cn'
+import { LinkIcon } from 'lucide-react'
 import type { ComponentPropsWithoutRef } from 'react'
 
 type Types = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
@@ -13,15 +14,13 @@ export function Heading<T extends Types = 'h1'>({
   const As = as ?? 'h1'
 
   return (
-    <As {...props} className={clsx('nd-scroll-m-20', props.className)}>
+    <As {...props} className={cn('nd-scroll-m-20', props.className)}>
       <a href={`#${props.id}`} className="nd-group nd-not-prose">
         {props.children}
-        <span
+        <LinkIcon
           aria-label="Link to section"
-          className="nd-ml-2 nd-font-medium nd-select-none nd-text-muted-foreground nd-opacity-0 nd-transition-opacity group-hover:nd-opacity-100"
-        >
-          #
-        </span>
+          className="nd-inline nd-w-4 nd-h-4 nd-ml-2 nd-text-muted-foreground nd-opacity-0 nd-transition-opacity group-hover:nd-opacity-100"
+        />
       </a>
     </As>
   )
