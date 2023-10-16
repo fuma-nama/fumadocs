2a7dc44e0e9b01b9874ccb38f40a28cc952b5ffe
SonMooSans <god63820869@gmail.com>
2023-10-16 18:22:41 +0800

    Mark shared dependencies as external

--- packages/next-docs/tsup.config.ts
+++ packages/next-docs/tsup.config.ts
@@ -6,7 +6,15 @@ export default defineConfig({
     'src/{server,breadcrumb,sidebar,toc,search,link,contentlayer,middleware,mdx-plugins,algolia,search-algolia}/index.{ts,tsx}',
     'src/contentlayer/configuration.ts'
   ],
-  external: ['contentlayer', 'algoliasearch'],
+  external: [
+    '@algolia/client-search',
+    '@types/react',
+    '@types/react-dom',
+    'algoliasearch',
+    'contentlayer',
+    'next',
+    'unified'
+  ],
   format: 'esm',
   dts: true,
   target: tsconfig.compilerOptions.target as 'es2016'
