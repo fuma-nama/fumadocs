c4919a3fe221da0ceb8508b9fe0b99ae8d11f395
SonMooSans <god63820869@gmail.com>
2023-10-20 19:48:22 +0800

    Zeta: Fix types exported as variables

--- packages/next-docs/src/mdx-plugins.ts
+++ packages/next-docs/src/mdx-plugins.ts
@@ -1,12 +1,15 @@
 export {
   default as rehypeImgSize,
-  Options as RehypeImgSizeOptions
+  type Options as RehypeImgSizeOptions
 } from 'rehype-img-size'
-export { default as remarkGfm, Options as RemarkGfmOptions } from 'remark-gfm'
+export {
+  default as remarkGfm,
+  type Options as RemarkGfmOptions
+} from 'remark-gfm'
 export { rehypeNextDocs } from './mdx-plugins/rehype-next-docs'
 export {
   remarkDynamicContent,
-  Options as RemarkDynamicContentOptions
+  type Options as RemarkDynamicContentOptions
 } from './mdx-plugins/remark-dynamic-content'
 export * from './mdx-plugins/remark-structure'
 export { remarkToc } from './mdx-plugins/remark-toc'
