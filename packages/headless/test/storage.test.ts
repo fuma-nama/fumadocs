fb1046d929f8e1249966ec9907d90edbe2815f6f
SonMooSans <god63820869@gmail.com>
2024-01-05 20:30:37 +0800

    Headless: Write unit tests for virtual storage

--- packages/headless/test/storage.test.ts
+++ packages/headless/test/storage.test.ts
@@ -1,6 +1,39 @@
 import { loader } from '@/source';
+import { Storage } from '@/source/file-system';
 import { parseFilePath, parseFolderPath } from '@/source/path';
 import { describe, expect, test } from 'vitest';
+
+describe('Virtual Storage', () => {
+  const storage = new Storage();
+
+  test('Writing', () => {
+    storage.write('test.mdx', {
+      type: 'page',
+      data: { title: 'Hello' },
+      slugs: ['test'],
+      url: '/test',
+    });
+    storage.write('meta.json', { type: 'meta', data: { pages: ['test'] } });
+
+    expect(storage.list().length).toBe(2);
+  });
+
+  test('Reading', () => {
+    expect(storage.read('test')).toBeDefined();
+    expect(storage.read('meta')).toBeDefined();
+  });
+
+  test('Nested Directories', () => {
+    storage.write('dir1/dir2/meta.json', {
+      type: 'meta',
+      data: { pages: ['test'] },
+    });
+
+    expect(storage.readDir('dir1')).toBeDefined();
+    expect(storage.readDir('dir1/dir2')).toBeDefined();
+    expect(storage.read('dir1/dir2/meta')).toBeDefined();
+  });
+});
 
 describe('Building File Graph', () => {
   test('Simple', () => {
@@ -41,60 +74,6 @@ describe('Building File Graph', () => {
   });
 
   test('Nested Directories', async () => {
-    loader({
-      source: {
-        files: [
-          {
-            type: 'page',
-            path: 'test.mdx',
-            data: {
-              title: 'Hello',
-            },
-          },
-          {
-            type: 'meta',
-            path: 'meta.json',
-            data: {
-              pages: ['test', 'nested'],
-            },
-          },
-          {
-            type: 'page',
-            path: '/nested/test.mdx',
-            data: {
-              title: 'Nested Page',
-            },
-          },
-        ],
-      },
-      transformers: [
-        ({ storage }) => {
-          expect(storage.root().children).toEqual([
-            expect.objectContaining({
-              type: 'page',
-              file: parseFilePath('test.mdx'),
-            }),
-            expect.objectContaining({
-              type: 'meta',
-              file: parseFilePath('meta.json'),
-            }),
-            expect.objectContaining({
-              type: 'folder',
-              file: parseFolderPath('nested'),
-              children: [
-                expect.objectContaining({
-                  type: 'page',
-                  file: parseFilePath('nested/test.mdx'),
-                }),
-              ],
-            }),
-          ]);
-        },
-      ],
-    });
-  });
-
-  test('Complicated Directories', async () => {
     loader({
       source: {
         files: [
