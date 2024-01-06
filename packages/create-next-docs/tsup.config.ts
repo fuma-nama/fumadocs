4b1338be0a7bc3498e74f45e119d13d56e9dd41d
SonMooSans <god63820869@gmail.com>
2024-01-06 17:15:05 +0800

    CLI: Convert to ESM

--- packages/create-next-docs/tsup.config.ts
+++ packages/create-next-docs/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['./src/index.ts'],
-  format: 'cjs',
+  entry: ['./src/index.ts', './src/create-app.ts'],
+  format: 'esm',
   target: 'node18',
 });
