a9f5dbcfad4c515358a9c6b000510a8ae730ac19
fuma-nama <god63820869@gmail.com>
2024-05-15 21:30:18 +0800

    Improve blog layout

--- apps/docs/app/(home)/blog/[slug]/page.client.tsx
+++ apps/docs/app/(home)/blog/[slug]/page.client.tsx
@@ -2,21 +2,34 @@
 import { ShareIcon } from 'lucide-react';
 import { cn } from '@/utils/cn';
 import { buttonVariants } from '@/components/ui/button';
+import {
+  TooltipContent,
+  Tooltip,
+  TooltipTrigger,
+} from '@radix-ui/react-tooltip';
+import { useState } from 'react';
 
 export function Control({ url }: { url: string }): React.ReactElement {
+  const [open, setOpen] = useState(false);
   const onClick = (): void => {
+    setOpen(true);
     void navigator.clipboard.writeText(`${window.location.origin}${url}`);
   };
 
   return (
-    <button
-      className={cn(
-        buttonVariants({ className: 'gap-2', variant: 'secondary' }),
-      )}
-      onClick={onClick}
-    >
-      <ShareIcon className="size-4" />
-      Share Post
-    </button>
+    <Tooltip open={open} onOpenChange={setOpen}>
+      <TooltipTrigger
+        className={cn(
+          buttonVariants({ className: 'gap-2', variant: 'secondary' }),
+        )}
+        onClick={onClick}
+      >
+        <ShareIcon className="size-4" />
+        Share Post
+      </TooltipTrigger>
+      <TooltipContent className="bg-popover p-2 text-popover-foreground text-sm border rounded-lg">
+        Copied
+      </TooltipContent>
+    </Tooltip>
   );
 }
