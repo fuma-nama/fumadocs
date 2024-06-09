ff6d47b48d9ff3d1cf00db5ec46df8de9b011bae
fuma-nama <god63820869@gmail.com>
2024-06-09 22:23:35 +0800

    Fix release problems

--- packages/create-app/.eslintrc.cjs
+++ packages/create-app/.eslintrc.cjs
@@ -1,4 +1,7 @@
 module.exports = {
   extends: ['custom/library'],
   ignorePatterns: ['/template/**/*'],
+  rules: {
+    'import/no-relative-packages': 'off',
+  },
 };
--- packages/create-app/tsup.config.ts
+++ packages/create-app/tsup.config.ts
@@ -1,4 +1,21 @@
+import { writeFileSync } from 'node:fs';
 import { defineConfig } from 'tsup';
+import corePkg from '../core/package.json';
+import uiPkg from '../ui/package.json';
+import mdxPkg from '../mdx/package.json';
+import contentlayerPkg from '../contentlayer/package.json';
+
+writeFileSync(
+  './versions.json',
+  JSON.stringify({
+    'fumadocs-core': corePkg.version,
+    'fumadocs-ui': uiPkg.version,
+    'fumadocs-mdx': mdxPkg.version,
+    'fumadocs-contentlayer': contentlayerPkg.version,
+  }),
+);
+
+console.log('version updated');
 
 export default defineConfig({
   entry: ['./src/index.ts', './src/create-app.ts'],
