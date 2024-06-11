af4419619ba0fb451794f873c11211df90ad7787
Ankur Goyal <ankrgyl@gmail.com>
2024-06-11 10:37:29 -0700

    Run prettier and udpate test snapshots

--- packages/openapi/src/generate.ts
+++ packages/openapi/src/generate.ts
@@ -123,7 +123,8 @@ function render(
       [
         '---',
         title && `title: ${title}`,
-        description && 'description: |\n  ' + description.split('\n').join('\n  ') + '\n',
+        description &&
+          'description: |\n  ' + description.split('\n').join('\n  ') + '\n',
         '---',
       ]
         .filter(Boolean)
