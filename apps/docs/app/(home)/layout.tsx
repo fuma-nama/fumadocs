1e6d1cf364611deb7d7bb6c2777949a5cc41d1cb
Lystrillian <webeugene@proton.me>
2024-06-21 04:56:24 +0300

    UI: New footer component for index page

--- apps/docs/app/(home)/layout.tsx
+++ apps/docs/app/(home)/layout.tsx
@@ -1,5 +1,6 @@
 import { Layout } from 'fumadocs-ui/layout';
 import type { ReactNode } from 'react';
+import Link from 'next/link';
 import { baseOptions } from '@/app/layout.config';
 
 export default function HomeLayout({
@@ -17,22 +18,26 @@ export default function HomeLayout({
 
 function Footer(): React.ReactElement {
   return (
-    <footer className="mt-auto border-t bg-card py-12 text-secondary-foreground">
-      <div className="container flex flex-col gap-4 sm:flex-row sm:items-end sm:justify-between">
-        <div>
-          <p className="mb-1 text-sm font-semibold">Fumadocs</p>
-          <p className="text-xs">
-            Built with ❤️ by{' '}
-            <a
-              href="https://fuma-dev.vercel.app"
-              rel="noreferrer noopener"
-              target="_blank"
-              className="font-medium"
-            >
-              Fuma
-            </a>
-          </p>
-        </div>
+    <footer className="py-6 md:px-8 md:py-0">
+      <div className="container flex flex-col items-center justify-center gap-4 md:h-16 md:flex-row">
+        <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
+          Built by{' '}
+          <Link
+            href="https://github.com/fuma-nama"
+            className="underline underline-offset-2 transition-colors hover:text-primary/90"
+            aria-label="Go to Fuma Nama's GitHub profile"
+          >
+            Fuma Nama
+          </Link>{' '}
+          . The source code is available on{' '}
+          <Link
+            href="https://github.com/fuma-nama/fumadocs"
+            className="underline underline-offset-2 transition-colors hover:text-primary/90"
+            aria-label="Go the the source code on GitHub"
+          >
+            GitHub
+          </Link>
+        </p>
       </div>
     </footer>
   );
