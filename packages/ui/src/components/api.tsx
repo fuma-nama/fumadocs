b4824fa267093682216cf7790e2d2798d52f7d5e
kyrylolvov <kyrylolvov@icloud.com>
2024-06-23 09:21:28 -0400

    CHANGED: - Linted project with Prettier - Added release note
    Signed-off-by: kyrylolvov <kyrylolvov@icloud.com>

--- packages/ui/src/components/api.tsx
+++ packages/ui/src/components/api.tsx
@@ -47,20 +47,23 @@ export interface APIInfoProps extends HTMLAttributes<HTMLDivElement> {
   route: string;
 }
 
-const badgeVariants = cva('rounded-lg border px-1.5 py-1 text-xs font-medium leading-[12px]', {
-  variants: {
-    color: {
-      green:
-        'border-green-400/50 bg-green-400/20 text-green-600 dark:text-green-400',
-      yellow:
-        'border-yellow-400/50 bg-yellow-400/20 text-yellow-600 dark:text-yellow-400',
-      red: 'border-red-400/50 bg-red-400/20 text-red-600 dark:text-red-400',
-      blue: 'border-blue-400/50 bg-blue-400/20 text-blue-600 dark:text-blue-400',
-      orange:
-        'border-orange-400/50 bg-orange-400/20 text-orange-600 dark:text-orange-400',
+const badgeVariants = cva(
+  'rounded-lg border px-1.5 py-1 text-xs font-medium leading-[12px]',
+  {
+    variants: {
+      color: {
+        green:
+          'border-green-400/50 bg-green-400/20 text-green-600 dark:text-green-400',
+        yellow:
+          'border-yellow-400/50 bg-yellow-400/20 text-yellow-600 dark:text-yellow-400',
+        red: 'border-red-400/50 bg-red-400/20 text-red-600 dark:text-red-400',
+        blue: 'border-blue-400/50 bg-blue-400/20 text-blue-600 dark:text-blue-400',
+        orange:
+          'border-orange-400/50 bg-orange-400/20 text-orange-600 dark:text-orange-400',
+      },
     },
   },
-});
+);
 
 export function APIInfo({
   children,
