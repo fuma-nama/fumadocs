9e6dc1f7a8ee65ff10480717007d2aed991c28fc
fuma-nama <god63820869@gmail.com>
2024-07-02 16:39:41 +0800

    Format code

--- examples/content-collections/next.config.mjs
+++ examples/content-collections/next.config.mjs
@@ -1,4 +1,4 @@
-import { withContentCollections } from "@content-collections/next";
+import { withContentCollections } from '@content-collections/next';
 
 /** @type {import('next').NextConfig} */
 const config = {
