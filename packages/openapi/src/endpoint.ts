ea15b78d62abc414035b0dd2f5e6f04834adce57
fuma-nama <god63820869@gmail.com>
2024-08-04 00:14:27 +0800

    OpenAPI: Improve generated sample requests

--- packages/openapi/src/endpoint.ts
+++ packages/openapi/src/endpoint.ts
@@ -1,15 +1,28 @@
 import type { OpenAPIV3 as OpenAPI } from 'openapi-types';
+import { sample } from 'openapi-sampler';
 import type { MethodInformation } from '@/types';
-import { getPreferredMedia, toSampleInput, noRef } from '@/utils/schema';
+import {
+  getPreferredMedia,
+  toSampleInput,
+  noRef,
+  getPreferredType,
+} from '@/utils/schema';
 import { generateInput } from '@/utils/generate-input';
 
+/**
+ * Sample info of endpoint
+ */
 export interface Endpoint {
   /**
-   * URL, including path and query parameters
+   * Request URL, including path and query parameters
    */
   url: string;
   method: string;
-  body?: unknown;
+  body?: {
+    schema: OpenAPI.SchemaObject;
+    mediaType: string;
+    sample: unknown;
+  };
   responses: Record<string, Response>;
   parameters: Parameter[];
 }
@@ -22,6 +35,7 @@ interface Parameter {
   name: string;
   in: string;
   schema: OpenAPI.SchemaObject;
+  sample: unknown;
 }
 
 export function createEndpoint(
@@ -33,21 +47,47 @@ export function createEndpoint(
   const responses: Endpoint['responses'] = {};
 
   for (const param of method.parameters) {
-    const schema = noRef(
-      param.schema ?? getPreferredMedia(param.content ?? {})?.schema,
-    );
+    if (param.schema) {
+      params.push({
+        name: param.name,
+        in: param.in,
+        schema: noRef(param.schema),
+        sample: param.example ?? sample(param.schema as object),
+      });
+    } else if (param.content) {
+      const key = getPreferredType(param.content);
+      const content = key ? param.content[key] : undefined;
 
-    if (!schema) continue;
+      if (!key || !content?.schema)
+        throw new Error(
+          `Cannot find parameter schema for ${param.name} in ${path} ${method.method}`,
+        );
 
-    params.push({
-      name: param.name,
-      in: param.in,
-      schema,
-    });
+      params.push({
+        name: param.name,
+        in: param.in,
+        schema: noRef(content.schema),
+        sample:
+          content.example ?? param.example ?? sample(content.schema as object),
+      });
+    }
   }
 
-  const body = noRef(method.requestBody)?.content ?? {};
-  const bodySchema = noRef(getPreferredMedia(body)?.schema);
+  let bodyOutput: Endpoint['body'];
+
+  if (method.requestBody) {
+    const body = noRef(method.requestBody).content;
+    const type = getPreferredType(body);
+    const schema = type ? noRef(body[type].schema) : undefined;
+    if (!type || !schema)
+      throw new Error(`Cannot find body schema for ${path} ${method.method}`);
+
+    bodyOutput = {
+      schema,
+      mediaType: type as string,
+      sample: body[type].example ?? generateInput(method.method, schema),
+    };
+  }
 
   for (const [code, value] of Object.entries(method.responses)) {
     const mediaTypes = noRef(value).content ?? {};
@@ -80,7 +120,7 @@ export function createEndpoint(
 
   return {
     url: new URL(`${baseUrl}${pathWithParameters}`).toString(),
-    body: bodySchema ? generateInput(method.method, bodySchema) : undefined,
+    body: bodyOutput,
     responses,
     method: method.method,
     parameters: params,
