95f8cded27940e49ec2a53910d7d9a99b7638026
fuma-nama <god63820869@gmail.com>
2024-09-06 17:49:55 +0800

    Docs: add `description`

--- examples/next-mdx/app/api/search/route.ts
+++ examples/next-mdx/app/api/search/route.ts
@@ -4,6 +4,7 @@ import { createSearchAPI } from 'fumadocs-core/search/server';
 export const { GET } = createSearchAPI('advanced', {
   indexes: source.getPages().map((page) => ({
     title: page.data.title,
+    description: page.data.description,
     structuredData: page.data.structuredData,
     id: page.url,
     url: page.url,
