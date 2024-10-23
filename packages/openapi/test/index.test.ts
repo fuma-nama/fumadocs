84563248fd4c3f978a7361ede40385abfb0a0dfe
Twinki <thetwinkehh@gmail.com>
2024-10-22 20:03:36 -0400

    cleanup

--- packages/openapi/test/index.test.ts
+++ packages/openapi/test/index.test.ts
@@ -87,7 +87,7 @@ describe('Generate documents', () => {
   test('Generate Files - groupBy tag per operation', async () => {
     await generateFiles({
       input: ['./fixtures/products.yaml'],
-      output: './out/products',
+      output: './out',
       per: 'operation',
       groupBy: 'tag',
       cwd,
@@ -98,21 +98,21 @@ describe('Generate documents', () => {
     expect(fs.writeFile).toBeCalledTimes(3);
 
     expect(fs.writeFile).toBeCalledWith(
-      join(cwd, './out/products/products/get-product-details.mdx'),
+      join(cwd, './out/products/get-product-details.mdx'),
       expect.anything(),
     );
 
     expect(fs.writeFile).toBeCalledWith(
-      join(cwd, './out/products/products/get-inventory-for-product.mdx'),
+      join(cwd, './out/products/get-inventory-for-product.mdx'),
       expect.anything(),
     );
 
     expect(fs.writeFile).toBeCalledWith(
-      join(cwd, './out/products/inventory/get-inventory-for-product.mdx'),
+      join(cwd, './out/inventory/get-inventory-for-product.mdx'),
       expect.anything(),
     );
 
-    expect(fs.mkdir).toBeCalledWith(join(cwd, './out/products/inventory'), expect.anything());
-    expect(fs.mkdir).toBeCalledWith(join(cwd, './out/products/products'), expect.anything());
+    expect(fs.mkdir).toBeCalledWith(join(cwd, './out/inventory'), expect.anything());
+    expect(fs.mkdir).toBeCalledWith(join(cwd, './out/products'), expect.anything());
   });
 });
