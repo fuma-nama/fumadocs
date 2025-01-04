2140aea90f6c3d2772b3a221542017b4dda95bd9
Fuma Nama <god63820869@gmail.com>
2025-01-04 10:11:00 +0800

    MDX: use different algorithm

--- packages/mdx/src/config/cached.ts
+++ packages/mdx/src/config/cached.ts
@@ -27,7 +27,7 @@ export async function loadConfigCached(
  * Generate hash based on the content of config
  */
 export async function getConfigHash(configPath: string): Promise<string> {
-  const hash = createHash('sha256');
+  const hash = createHash('md5');
   const rs = fs.createReadStream(configPath);
 
   for await (const chunk of rs) {
