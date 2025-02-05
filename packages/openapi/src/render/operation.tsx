cfd7d6de2f6cc68f844cba989d9a392bdbd3e789
Michael Ermer <m.ermer@abillify.me>
2025-02-05 14:52:59 +0100

    fix(packages/openapi): hide AuthSection is security is an empty array

--- packages/openapi/src/render/operation.tsx
+++ packages/openapi/src/render/operation.tsx
@@ -203,7 +203,7 @@ export function Operation({
       {type === 'operation' ? (
         <ctx.renderer.APIPlayground path={path} method={method} ctx={ctx} />
       ) : null}
-      {security ? (
+      {security && Object.keys(security).length ? (
         <>
           {heading(headingLevel, 'Authorization', ctx)}
           <AuthSection requirements={security} ctx={ctx} />
