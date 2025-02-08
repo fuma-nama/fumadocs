c735dcdd675365752b6ddb706bd016d19ce4165b
Fuma Nama <god63820869@gmail.com>
2025-02-08 23:02:34 +0800

    Docs: Improve MDX section

--- apps/docs/source.config.ts
+++ apps/docs/source.config.ts
@@ -48,46 +48,44 @@ export const blog = defineCollections({
 
 export default defineConfig({
   lastModifiedTime: 'git',
-  mdxOptions: async () => {
-    return {
-      rehypeCodeOptions: {
-        lazy: true,
-        experimentalJSEngine: true,
-        langs: ['ts', 'js', 'html', 'tsx', 'mdx'],
-        inline: 'tailing-curly-colon',
-        themes: {
-          light: 'catppuccin-latte',
-          dark: 'catppuccin-mocha',
-        },
-        transformers: [
-          ...(rehypeCodeDefaultOptions.transformers ?? []),
-          transformerTwoslash(),
-          {
-            name: 'transformers:remove-notation-escape',
-            code(hast) {
-              for (const line of hast.children) {
-                if (line.type !== 'element') continue;
-
-                const lastSpan = line.children.findLast(
-                  (v) => v.type === 'element',
-                );
-
-                const head = lastSpan?.children[0];
-                if (head?.type !== 'text') return;
-
-                head.value = head.value.replace(/\[\\!code/g, '[!code');
-              }
-            },
-          },
-        ],
+  mdxOptions: {
+    rehypeCodeOptions: {
+      lazy: true,
+      experimentalJSEngine: true,
+      langs: ['ts', 'js', 'html', 'tsx', 'mdx'],
+      inline: 'tailing-curly-colon',
+      themes: {
+        light: 'catppuccin-latte',
+        dark: 'catppuccin-mocha',
       },
-      remarkPlugins: [
-        remarkMath,
-        [remarkInstall, { persist: { id: 'package-manager' } }],
-        [remarkDocGen, { generators: [fileGenerator()] }],
-        remarkTypeScriptToJavaScript,
+      transformers: [
+        ...(rehypeCodeDefaultOptions.transformers ?? []),
+        transformerTwoslash(),
+        {
+          name: 'transformers:remove-notation-escape',
+          code(hast) {
+            for (const line of hast.children) {
+              if (line.type !== 'element') continue;
+
+              const lastSpan = line.children.findLast(
+                (v) => v.type === 'element',
+              );
+
+              const head = lastSpan?.children[0];
+              if (head?.type !== 'text') return;
+
+              head.value = head.value.replace(/\[\\!code/g, '[!code');
+            }
+          },
+        },
       ],
-      rehypePlugins: (v) => [rehypeKatex, ...v],
-    };
+    },
+    remarkPlugins: [
+      remarkMath,
+      [remarkInstall, { persist: { id: 'package-manager' } }],
+      [remarkDocGen, { generators: [fileGenerator()] }],
+      remarkTypeScriptToJavaScript,
+    ],
+    rehypePlugins: (v) => [rehypeKatex, ...v],
   },
 });
