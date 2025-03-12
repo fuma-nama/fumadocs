768ba65edf4b63205ac635072f21fd7fd3599567
Fuma Nama <god63820869@gmail.com>
2025-03-12 17:38:16 +0800

    UI: Improve accordion

--- packages/create-app/template/fuma-docs-mdx/lib/source.ts
+++ packages/create-app/template/fuma-docs-mdx/lib/source.ts
@@ -1,8 +1,9 @@
 import { docs } from '@/.source';
 import { loader } from 'fumadocs-core/source';
 
+// `loader()` also assign a URL to your pages
+// See https://fumadocs.vercel.app/docs/headless/source-api for more info
 export const source = loader({
-  // `loader()` also assign an url to your pages
   baseUrl: '/docs',
   source: docs.toFumadocsSource(),
 });
