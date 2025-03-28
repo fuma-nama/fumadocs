6e844e3a1b583616b759f2820fbb9a10250b7452
Fuma Nama <god63820869@gmail.com>
2025-03-28 19:11:11 +0800

    Remove Astro experiment

--- apps/astro/src/layouts/layout.tsx
+++
@@ -1,8 +0,0 @@
-import { type ReactNode, useEffect } from 'react';
-
-export function Layout({ children }: { children: ReactNode }) {
-  useEffect(() => {
-    console.log('Layout');
-  }, []);
-  return <div>{children}</div>;
-}
