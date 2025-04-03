9837082ed44958007f9ca98e1f904c74a3fa9ef3
Fuma Nama <76240755+fuma-nama@users.noreply.github.com>
2025-04-03 16:44:00 +0800

    Update route-tag.ts

--- examples/next-mdx/app/api/search/route-tag.ts
+++ examples/next-mdx/app/api/search/route-tag.ts
@@ -7,5 +7,6 @@ export const { GET } = createFromSource(source, (page) => ({
   url: page.url,
   id: page.url,
   structuredData: page.data.structuredData,
-  tag: "<value>" // page.slugs[0]
+  // use your desired value, like page.slugs[0]
+  tag: "<value>"
 }));
