2bfef878ac8d430600dfd14788aa36d290cc2e06
Fuma Nama <god63820869@gmail.com>
2025-04-07 16:11:49 +0800

    deploy Tanstack Start to Vercel

--- examples/tanstack-start/app.config.ts
+++ examples/tanstack-start/app.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   server: {
+    preset: 'vercel',
     hooks: {
       'prerender:routes': async (routes) => {
         const { source } = await import('./lib/source');
