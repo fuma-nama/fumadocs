0fa211ddead7f12c60d21aa095e5d705671c6b6a
Fuma Nama <god63820869@gmail.com>
2025-04-12 18:21:32 +0800

    Docs: prefer custom config for og image generation

--- examples/next-mdx/app/docs/[[...slug]]/page.with-og-image.tsx
+++ examples/next-mdx/app/docs/[[...slug]]/page.with-og-image.tsx
@@ -7,6 +7,7 @@ import {
 } from 'fumadocs-ui/page';
 import { notFound } from 'next/navigation';
 import { getMDXComponents } from '@/mdx-components';
+import type { Metadata } from 'next';
 
 export default async function Page(props: {
   params: Promise<{ slug?: string[] }>;
@@ -32,15 +33,25 @@ export async function generateStaticParams() {
   return source.generateParams();
 }
 
-export async function generateMetadata(props: {
+export async function generateMetadata({
+  params,
+}: {
   params: Promise<{ slug?: string[] }>;
 }) {
-  const params = await props.params;
-  const page = source.getPage(params.slug);
+  const { slug = [] } = await params;
+  const page = source.getPage(slug);
   if (!page) notFound();
 
+  const image = ['/docs-og', ...slug, 'image.png'].join('/');
   return {
     title: page.data.title,
     description: page.data.description,
-  };
+    openGraph: {
+      images: image,
+    },
+    twitter: {
+      card: 'summary_large_image',
+      images: image,
+    },
+  } satisfies Metadata;
 }
