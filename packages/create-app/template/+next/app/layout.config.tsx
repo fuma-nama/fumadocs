af774c56e2ce783d4c01ada00e3f75d3f2ba113c
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-04-16 14:32:00 +0800

    fix: typo

--- packages/create-app/template/+next/app/layout.config.tsx
+++ packages/create-app/template/+next/app/layout.config.tsx
@@ -3,7 +3,7 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 /**
  * Shared layout configurations
  *
- * you can customise layouts individually from:
+ * you can customize layouts individually from:
  * Home Layout: app/(home)/layout.tsx
  * Docs Layout: app/docs/layout.tsx
  */
