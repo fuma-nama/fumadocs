124cc0c6b7ceae3566f3fd91ebbae09de377a2c1
Fuma Nama <god63820869@gmail.com>
2025-05-04 20:53:56 +0800

    Docs: Add `llms-full.txt`

--- apps/docs/app/llms.txt/route.ts
+++ apps/docs/app/llms.txt/route.ts
@@ -1,14 +1,23 @@
 import { source } from '@/lib/source';
-import { getLLMText } from '@/lib/get-llm-text';
 
 export const revalidate = false;
 
 export async function GET() {
-  const scan = source
-    .getPages()
-    .filter((file) => file.slugs[0] !== 'openapi')
-    .map(getLLMText);
-  const scanned = await Promise.all(scan);
+  const scanned: string[] = [];
+  scanned.push('# Docs');
+  const map = new Map<string, string[]>();
+
+  for (const page of source.getPages()) {
+    const dir = page.slugs[0];
+    const list = map.get(dir) ?? [];
+    list.push(`- [${page.data.title}](${page.url}): ${page.data.description}`);
+    map.set(dir, list);
+  }
+
+  for (const [key, value] of map) {
+    scanned.push(`## ${key}`);
+    scanned.push(value.join('\n'));
+  }
 
   return new Response(scanned.join('\n\n'));
 }
