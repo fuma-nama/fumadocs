a0fb1ec651eba2f0bfd163000af9cd09c994d231
STACiA <stacia.dev@gmail.com>
2025-05-16 04:01:52 +0700

    chore:  __all__ declaration for improved readability

--- packages/python/fumapy/mksource/__init__.py
+++ packages/python/fumapy/mksource/__init__.py
@@ -1,2 +1,10 @@
 from .document_module import parse_class, parse_function, parse_module
 from .json_encoder import CustomEncoder
+
+
+__all__ = (
+    "parse_class",
+    "parse_function",
+    "parse_module",
+    "CustomEncoder",
+)
