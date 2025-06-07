38d851c3f7487e4e013fe1ff8970bdc2faee92ba
Fuma Nama <god63820869@gmail.com>
2025-06-07 20:19:29 +0800

    fix examples & bump deps

--- packages/create-app/template/tanstack-start/app/routes/__root.tsx
+++ packages/create-app/template/tanstack-start/app/routes/__root.tsx
@@ -1,9 +1,13 @@
 import { type ReactNode } from 'react';
-import { Outlet, createRootRoute, HeadContent } from '@tanstack/react-router';
+import {
+  createRootRoute,
+  HeadContent,
+  Outlet,
+  Scripts,
+} from '@tanstack/react-router';
 import { RootProvider } from 'fumadocs-ui/provider/base';
 import appCss from '../app.css?url';
 import { TanstackProvider } from 'fumadocs-core/framework/tanstack';
-import { Scripts } from '@tanstack/react-router';
 
 export const Route = createRootRoute({
   head: () => ({
@@ -38,7 +42,7 @@ function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
       <head>
         <HeadContent />
       </head>
-      <body>
+      <body className="flex flex-col min-h-screen">
         <TanstackProvider>
           <RootProvider>{children}</RootProvider>
         </TanstackProvider>
