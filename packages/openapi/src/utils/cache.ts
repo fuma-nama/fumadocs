ba94f3ce51aed45b9d4417c5c252638e024ac8eb
remorses <beats.by.morse@gmail.com>
2025-07-11 10:27:14 +0200

    use LRUCache for the client cache

--- packages/openapi/src/utils/cache.ts
+++ packages/openapi/src/utils/cache.ts
@@ -1,9 +1,13 @@
+import { LRUCache } from 'lru-cache';
+
 export function cache<T extends (...args: any[]) => any>(fn: T): T {
-  const memo = new Map<string, ReturnType<T>>();
+  const memo = new LRUCache<string, string | object>({
+    max: 200,
+  });
   return function (...args: Parameters<T>): ReturnType<T> {
     const key = JSON.stringify(args);
     if (memo.has(key)) {
-      return memo.get(key)!;
+      return memo.get(key) as ReturnType<T>;
     }
     const result = fn(...args);
     memo.set(key, result);
