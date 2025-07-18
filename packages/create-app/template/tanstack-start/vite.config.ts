619806da8b4df299deb30b1d5a5bf55ede997b3a
Ray <153027766+RMNCLDYO@users.noreply.github.com>
2025-07-17 20:37:38 -0400

    Fix Vite and Tanstack Router configuration warnings (#2092)

--- packages/create-app/template/tanstack-start/vite.config.ts
+++ packages/create-app/template/tanstack-start/vite.config.ts
@@ -2,6 +2,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   server: {
@@ -12,6 +13,9 @@ export default defineConfig({
     tsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
-    tanstackStart(),
+    tanstackStart({
+      customViteReactPlugin: true,
+    }),
+    react(),
   ],
 });
