526fd82f6c9ee006c2cbf53972d1284480b8c2a3
Fuma Nama <god63820869@gmail.com>
2025-07-21 21:13:46 +0800

    CLI: Update Tanstack Start & React Router templates to use Fumadocs MDX

--- packages/create-app/template/tanstack-start/vite.config.ts
+++ packages/create-app/template/tanstack-start/vite.config.ts
@@ -1,14 +1,16 @@
+import react from '@vitejs/plugin-react';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
+import mdx from 'fumadocs-mdx/vite';
 
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [
+    mdx(await import('./source.config')),
     tailwindcss(),
     tsConfigPaths({
       projects: ['./tsconfig.json'],
