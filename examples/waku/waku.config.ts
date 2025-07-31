4cf1ed475633c419b2e3e8932b7038f6b09114a9
Alex Yang <himself65@outlook.com>
2025-07-30 22:42:55 -0700

    docs: add

--- examples/waku/waku.config.ts
+++ examples/waku/waku.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'waku/config';
 import mdx from 'fumadocs-mdx/vite';
 import * as MdxConfig from './source.config.js';
+import { UserConfig } from 'vite';
 
 export default defineConfig({
   unstable_viteConfigs: {
-    //@ts-expect-error
     common() {
       return {
         plugins: [mdx(MdxConfig)],
-      };
+      } as UserConfig;
     },
   },
 });
