5ec8c38a39ac32f6db48fa5c1084baba80bd5d0b
Fuma Nama <god63820869@gmail.com>
2025-08-01 19:04:33 +0800

    remove generated files from OpenAPI example

--- examples/waku-openapi/scripts/generate-docs.ts
+++ examples/waku-openapi/scripts/generate-docs.ts
@@ -2,6 +2,6 @@ import { generateFiles } from 'fumadocs-openapi';
 
 void generateFiles({
   input: ['https://cdn.jsdelivr.net/npm/@scalar/galaxy/dist/latest.yaml'],
-  output: './content/docs',
+  output: './content/docs/(generated)',
   includeDescription: true,
 });
