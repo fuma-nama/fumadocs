68eb256b509596b1c7db350f62fa79b867d40842
Fuma Nama <god63820869@gmail.com>
2025-08-04 21:51:08 +0800

    format

--- packages/openapi/src/ui/lazy.tsx
+++ packages/openapi/src/ui/lazy.tsx
@@ -1,17 +1,13 @@
 'use client';
-import {
-  type ComponentProps,
-  type ComponentType,
-  lazy,
-  type LazyExoticComponent,
-} from 'react';
+import { type ComponentType, lazy } from 'react';
+import type { JSX } from 'react/jsx-runtime';
 
 // Waku wraps all export functions of a "use client" file in `React.lazy`, but `lazy(lazy(() => ...))` causes error.
 // we wrap another layer of component such that it is valid
 // TODO: perhaps we can remove it once Waku migrated to vite-rsc
-function wrap<C extends LazyExoticComponent<ComponentType<any>>>(V: C) {
-  return function wrapper(props: ComponentProps<C>) {
-    return <V {...props} />;
+function wrap<Props>(V: ComponentType<Props>) {
+  return function wrapper(props: Props) {
+    return <V {...(props as Props & JSX.IntrinsicAttributes)} />;
   };
 }
 
