bdef284c4d60eaecd07a8ef6915ea9bcfe527be6
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-08-09 19:47:02 +0200

    fix: ensure metadata points to production url (#2180)

--- apps/docs/lib/metadata.ts
+++ apps/docs/lib/metadata.ts
@@ -34,6 +34,6 @@ export function createMetadata(override: Metadata): Metadata {
 }
 
 export const baseUrl =
-  process.env.NODE_ENV === 'development' || !process.env.VERCEL_URL
+  process.env.NODE_ENV === 'development' || !process.env.VERCEL_PROJECT_PRODUCTION_URL
     ? new URL('http://localhost:3000')
-    : new URL(`https://${process.env.VERCEL_URL}`);
+    : new URL(`https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`);
