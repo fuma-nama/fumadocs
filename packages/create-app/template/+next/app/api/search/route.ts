9ee9fb01a529d3126fc0a14fd56a56e245b140ad
Mikiyas Tibebu <mikitebe123@gmail.com>
2025-08-12 13:51:56 +0300

    Update route.ts

--- packages/create-app/template/+next/app/api/search/route.ts
+++ packages/create-app/template/+next/app/api/search/route.ts
@@ -2,6 +2,6 @@ import { source } from '@/lib/source';
 import { createFromSource } from 'fumadocs-core/search/server';
 
 export const { GET } = createFromSource(source, {
-  // https://docs.orama.com/open-source/supported-languages
+  // https://docs.orama.com/docs/orama-js/supported-languages
   language: 'english',
 });
