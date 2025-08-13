83ba59cf3d558f871905d127d7df5a7119c060b4
Fuma Nama <god63820869@gmail.com>
2025-08-13 18:21:38 +0800

    Docs: improve wrapper styles

--- apps/docs/components/preview/wrapper.tsx
+++ apps/docs/components/preview/wrapper.tsx
@@ -6,7 +6,7 @@ export function Wrapper(props: HTMLAttributes<HTMLDivElement>) {
     <div
       {...props}
       className={cn(
-        'rounded-lg bg-radial-[at_bottom] from-blue-500/20 p-4 border border-fd-primary/10 prose-no-margin dark:bg-black/20',
+        'rounded-lg bg-radial-[at_bottom] from-blue-500/20 p-4 border bg-origin-border border-fd-primary/10 prose-no-margin dark:bg-black/20',
         props.className,
       )}
     >
