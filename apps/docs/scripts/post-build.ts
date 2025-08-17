80cf0d8cbcb06cdd92af17cf174e6ddb37d0d99e
Fuma Nama <god63820869@gmail.com>
2025-08-17 18:20:45 +0800

    Chore: use bun for scripts

--- apps/docs/scripts/post-build.ts
+++ apps/docs/scripts/post-build.ts
@@ -1,5 +1,5 @@
 import env from '@next/env';
-import { updateSearchIndexes } from './update-orama-index.mjs';
+import { updateSearchIndexes } from './update-orama-index';
 
 env.loadEnvConfig(process.cwd());
 
