8eae1cb736b6122e59fba97c55781a38c2f78f7a
Fuma Nama <god63820869@gmail.com>
2025-09-20 17:44:30 +0800

    update snapshots

--- packages/openapi/src/generate-file.ts
+++ packages/openapi/src/generate-file.ts
@@ -481,7 +481,9 @@ function writeIndexFiles(context: HookContext, options: Config) {
       const { data } = matter(file.content);
       if (typeof data.title !== 'string') continue;
 
-      const descriptionAttr = data.description ? `description=${JSON.stringify(data.description)} ` : '';
+      const descriptionAttr = data.description
+        ? `description=${JSON.stringify(data.description)} `
+        : '';
       content.push(
         `<Card href="${urlFn(file.path)}" title=${JSON.stringify(data.title)} ${descriptionAttr}/>`,
       );
