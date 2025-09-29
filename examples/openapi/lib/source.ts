5861cbcd00941eb9c970858355598ff3656b7d86
Saatvik Arya <aryasaatvik@gmail.com>
2025-09-29 08:21:59 +0530

    chore: point createMDXSource imports to runtime/next (#2407)
    * chore: point createMDXSource imports to runtime/next
    
    * fix: update import path for next runtime in MDX index to maintian backwards compat

--- examples/openapi/lib/source.ts
+++ examples/openapi/lib/source.ts
@@ -1,4 +1,4 @@
-import { createMDXSource } from 'fumadocs-mdx';
+import { createMDXSource } from 'fumadocs-mdx/runtime/next';
 import { loader } from 'fumadocs-core/source';
 import { openapiPlugin } from 'fumadocs-openapi/server';
 import { docs, meta } from '@/.source';
