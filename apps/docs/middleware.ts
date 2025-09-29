7a248dbcc73495ebe02cc408fddb92c2af720e48
Saatvik Arya <aryasaatvik@gmail.com>
2025-09-29 09:32:58 +0530

    feat: add markdown middleware for docs and blog routes

---
+++ apps/docs/middleware.ts
@@ -0,0 +1,13 @@
+import { createMarkdownMiddleware } from 'fumadocs-core/http/middleware/next';
+
+export const middleware = createMarkdownMiddleware({
+  resolver: {
+    redirectOptions: {
+      markdownExtension: '.mdx',
+    },
+  },
+});
+
+export const config = {
+  matcher: ['/docs/:path*', '/blog/:path*'],
+}
