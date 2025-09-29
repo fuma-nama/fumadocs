f1a2616f71fc856de02551f5bd0601abddc263f2
Saatvik Arya <aryasaatvik@gmail.com>
2025-09-29 07:59:30 +0530

    fix: update import path for next runtime in MDX index to maintian backwards compat

--- packages/mdx/src/index.ts
+++ packages/mdx/src/index.ts
@@ -2,6 +2,6 @@ export type * from './runtime/shared';
 
 // backward compat
 // TODO: require importing `fumadocs-mdx/runtime/next` instead
-export * from './next';
+export * from './runtime/next';
 
 export type { ExtractedReference } from '@/loaders/mdx/remark-postprocess';
