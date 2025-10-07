4049cccde62f65c5f6bbedfb96bcf275d9e1234e
Fuma Nama <god63820869@gmail.com>
2025-10-07 12:07:34 +0800

    Core: Remove `fumadocs-core/server` export

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   format: 'esm',
   entry: [
     'src/{toc,link,breadcrumb,sidebar,dynamic-link,hide-if-empty}.tsx',
-    'src/{server,source,mdx-plugins,page-tree}/index.ts',
+    'src/{source,mdx-plugins,page-tree}/index.ts',
     'src/{negotiation,content}/*',
     'src/source/plugins/lucide-icons.ts',
     'src/search/{index,client,server,algolia,orama-cloud}.ts',
