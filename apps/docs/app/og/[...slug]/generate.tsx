04056232c77d08cad7d11eb6a9c80b6c0d304958
Kane Wang <kane@yeecord.com>
2025-11-11 04:40:19 +0800

    Docs: fix takumi font loading crashed in build worker

--- apps/docs/app/og/[...slug]/generate.tsx
+++ apps/docs/app/og/[...slug]/generate.tsx
@@ -1,31 +1,29 @@
 import type { ReactNode } from 'react';
 import fs from 'node:fs/promises';
-import type { ImageResponseOptions } from 'next/server';
+import type { ImageResponseOptions } from '@takumi-rs/image-response';
 
 export interface GenerateProps {
   title: ReactNode;
   description?: ReactNode;
 }
 
-const font = fs.readFile('./lib/og/JetBrainsMono-Regular.ttf');
-const fontBold = fs.readFile('./lib/og/JetBrainsMono-Bold.ttf');
+const font = fs.readFile('./lib/og/JetBrainsMono-Regular.ttf').then(data => ({
+  name: 'Mono',
+  data,
+  weight: 400,
+}));
+const fontBold = fs.readFile('./lib/og/JetBrainsMono-Bold.ttf').then(data => ({
+  name: 'Mono',
+  data,
+  weight: 600,
+}));
 
 export async function getImageResponseOptions(): Promise<ImageResponseOptions> {
   return {
     width: 1200,
     height: 630,
-    fonts: [
-      {
-        name: 'Mono',
-        data: await font,
-        weight: 400,
-      },
-      {
-        name: 'Mono',
-        data: await fontBold,
-        weight: 600,
-      },
-    ],
+    format: 'webp',
+    fonts: await Promise.all([font, fontBold]),
   };
 }
 
