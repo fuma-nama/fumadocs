ef8eb6c36175cba7d266504637e61c618d17e255
Fuma Nama <god63820869@gmail.com>
2025-12-04 16:16:08 +0800

    Core: Expose Zod schema for page & meta data

---
+++ .changeset/blue-ads-rule.md
@@ -0,0 +1,5 @@
+---
+'fumadocs-core': patch
+---
+
+Expose Zod schema for page & meta data
---
+++ packages/core/src/source/schema.ts
@@ -0,0 +1,27 @@
+import { z } from 'zod';
+
+/**
+ * Zod 4 schema
+ */
+export const metaSchema = z.object({
+  title: z.string().optional(),
+  pages: z.array(z.string()).optional(),
+  description: z.string().optional(),
+  root: z.boolean().optional(),
+  defaultOpen: z.boolean().optional(),
+  collapsible: z.boolean().optional(),
+  icon: z.string().optional(),
+});
+
+/**
+ * Zod 4 schema
+ */
+export const pageSchema = z.object({
+  title: z.string(),
+  description: z.string().optional(),
+  icon: z.string().optional(),
+  full: z.boolean().optional(),
+
+  // Fumadocs OpenAPI generated
+  _openapi: z.looseObject({}).optional(),
+});
