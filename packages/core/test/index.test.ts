d17499b40e2bba3364f1827c928ac63b8dea5a33
Fuma Nama <god63820869@gmail.com>
2025-12-09 18:13:55 +0800

    Core: Fix `basePath` being ignored

--- packages/core/test/index.test.ts
+++ packages/core/test/index.test.ts
@@ -3,6 +3,8 @@ import { describe, expect, test } from 'vitest';
 import type { Root } from '@/page-tree/definitions';
 import { findNeighbour } from '@/page-tree/utils';
 import { getBreadcrumbItems } from '@/breadcrumb';
+import { DefaultFormatter } from '@/i18n/middleware';
+import { NextURL } from 'next/dist/server/web/next-url';
 
 test('Find Neighbours', () => {
   const tree: Root = {
@@ -93,4 +95,65 @@ test('Breadcrumbs', () => {
   ).toStrictEqual([{ name: 'World', url: '/docs/folder' }]);
 
   expect(getBreadcrumbItems('/invalid', tree)).toMatchInlineSnapshot(`[]`);
+});
+
+test('I18n: Format URL', () => {
+  expect(DefaultFormatter.get(new NextURL('https://fumadocs.dev/en'))).toBe(
+    'en',
+  );
+  expect(
+    DefaultFormatter.get(new NextURL('https://fumadocs.dev/en/test')),
+  ).toBe('en');
+  expect(
+    DefaultFormatter.get(new NextURL('https://fumadocs.dev')),
+  ).toBeUndefined();
+  expect(
+    DefaultFormatter.get(
+      new NextURL('https://fumadocs.dev/docs', {
+        nextConfig: {
+          basePath: '/docs',
+        },
+      }),
+    ),
+  ).toBeUndefined();
+  expect(
+    DefaultFormatter.get(
+      new NextURL('https://fumadocs.dev/docs/en/test', {
+        nextConfig: {
+          basePath: '/docs',
+        },
+      }),
+    ),
+  ).toBe('en');
+
+  expect(
+    DefaultFormatter.add(new NextURL('https://fumadocs.dev'), 'cn').href,
+  ).toBe('https://fumadocs.dev/cn/');
+  expect(
+    DefaultFormatter.add(
+      new NextURL('https://fumadocs.dev/docs', {
+        nextConfig: {
+          basePath: '/docs',
+        },
+      }),
+      'cn',
+    ).href,
+  ).toBe('https://fumadocs.dev/docs/cn/');
+
+  expect(
+    DefaultFormatter.remove(new NextURL('https://fumadocs.dev/en')).href,
+  ).toBe('https://fumadocs.dev/');
+  expect(
+    DefaultFormatter.remove(new NextURL('https://fumadocs.dev/en/test/hello'))
+      .href,
+  ).toBe('https://fumadocs.dev/test/hello');
+  expect(
+    DefaultFormatter.remove(
+      new NextURL('https://fumadocs.dev/docs/en', {
+        nextConfig: {
+          basePath: '/docs',
+        },
+      }),
+    ).href,
+  ).toBe('https://fumadocs.dev/docs/');
 });
