ed020e3dcf8d03e83e1fef26cc54782d5934368d
Fuma Nama <god63820869@gmail.com>
2025-12-19 19:48:02 +0800

    OpenAPI: reduce deps

--- packages/openapi/src/ui/operation/client.tsx
+++ packages/openapi/src/ui/operation/client.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useCopyButton } from '@fumadocs/ui/hooks/use-copy-button';
+import { useCopyButton } from 'fumadocs-ui/utils/use-copy-button';
 import { buttonVariants } from 'fumadocs-ui/components/ui/button';
 import { cn } from '@/utils/cn';
 import { Check, Copy } from '@/ui/icons';
