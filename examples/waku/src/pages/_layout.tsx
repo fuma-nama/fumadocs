f150307826a6439eb4317a92a070594a99b6ff93
Fuma Nama <god63820869@gmail.com>
2026-01-04 16:24:33 +0800

    Waku: fix default config

--- examples/waku/src/pages/_layout.tsx
+++
@@ -1,7 +0,0 @@
-import '@/styles/globals.css';
-import type { ReactNode } from 'react';
-import { Provider } from '@/components/provider';
-
-export default function RootLayout({ children }: { children: ReactNode }) {
-  return <Provider>{children}</Provider>;
-}
