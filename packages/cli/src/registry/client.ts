e7e4a729ca1efd2c22dcf4319b7c99a9f9f1cb94
Fuma Nama <god63820869@gmail.com>
2026-01-05 23:27:31 +0800

    CLI: fix cache

--- packages/cli/src/registry/client.ts
+++ packages/cli/src/registry/client.ts
@@ -8,7 +8,7 @@ import path from 'node:path';
 import fs from 'node:fs/promises';
 import type { LoadedConfig } from '@/config';
 import { log } from '@clack/prompts';
-import { AsyncCache } from '@/utils/cache';
+import { createCache } from '@/utils/cache';
 
 export interface RegistryClient {
   readonly registryId: string;
@@ -19,7 +19,7 @@ export interface RegistryClient {
   createLinkedRegistryClient: (registryName: string) => RegistryClient;
 }
 
-const fetchCache = new AsyncCache<unknown>();
+const fetchCache = createCache<unknown>();
 
 export class HttpRegistryClient implements RegistryClient {
   readonly registryId: string;
@@ -34,7 +34,7 @@ export class HttpRegistryClient implements RegistryClient {
   async fetchRegistryInfo(baseUrl = this.baseUrl) {
     const url = new URL('_registry.json', `${baseUrl}/`);
 
-    return fetchCache.cached<DownloadedRegistryInfo>(url.href, async () => {
+    return fetchCache.$value<DownloadedRegistryInfo>().cached(url.href, async () => {
       const res = await fetch(url);
       if (!res.ok) {
         throw new Error(`failed to fetch ${url.href}: ${res.statusText}`);
@@ -47,7 +47,7 @@ export class HttpRegistryClient implements RegistryClient {
   async fetchComponent(name: string) {
     const url = new URL(`${name}.json`, `${this.baseUrl}/`);
 
-    return fetchCache.cached<Component>(url.href, async () => {
+    return fetchCache.$value<Component>().cached(url.href, async () => {
       const res = await fetch(`${this.baseUrl}/${name}.json`);
       if (!res.ok) {
         log.error(`component ${name} not found at ${url.href}`);
