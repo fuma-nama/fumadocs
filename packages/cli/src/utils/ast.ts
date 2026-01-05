6039041d040487f3dacc86f4e440718f45270262
Fuma Nama <god63820869@gmail.com>
2026-01-05 23:18:36 +0800

    CLI: Migrate to oxc for AST manipulation

--- packages/cli/src/utils/ast.ts
+++ packages/cli/src/utils/ast.ts
@@ -1,5 +1,7 @@
 import path from 'node:path';
 import { typescriptExtensions } from '@/constants';
+import { type Program, Visitor } from 'oxc-parser';
+import type MagicString from 'magic-string';
 
 /**
  * Return the import modifier for `sourceFile` to import `referenceFile`
@@ -26,4 +28,51 @@ export function toImportSpecifier(sourceFile: string, referenceFile: string): st
   }
 
   return importPath.startsWith('../') ? importPath : `./${importPath}`;
+}
+
+export function transformSpecifiers(
+  program: Program,
+  s: MagicString,
+  transformSpecifier: (value: string) => string | undefined,
+) {
+  new Visitor({
+    // static imports
+    ImportDeclaration(node) {
+      const source = node.source;
+      const out = transformSpecifier(source.value);
+      if (out) {
+        s.update(source.start + 1, source.end - 1, out);
+        source.value = out;
+      }
+    },
+    // dynamic imports
+    ImportExpression(node) {
+      if (node.source.type === 'Literal') {
+        const source = node.source;
+        const out = transformSpecifier(source.value as string);
+        if (out) {
+          s.update(source.start + 1, source.end - 1, out);
+          source.value = out;
+        }
+      }
+    },
+    // exports
+    ExportAllDeclaration(node) {
+      const source = node.source;
+      const out = transformSpecifier(source.value);
+      if (out) {
+        s.update(source.start + 1, source.end - 1, out);
+        source.value = out;
+      }
+    },
+    ExportNamedDeclaration(node) {
+      const source = node.source;
+      if (!source) return;
+      const out = transformSpecifier(source.value);
+      if (out) {
+        s.update(source.start + 1, source.end - 1, out);
+        source.value = out;
+      }
+    },
+  }).visit(program);
 }
--- packages/cli/src/utils/cache.ts
+++ packages/cli/src/utils/cache.ts
@@ -1,12 +1,47 @@
-export class AsyncCache<V> {
-  readonly store = new Map<string, V | Promise<V>>();
+export interface AsyncCache<V> {
+  cached: (
+    key: string,
+    fn: (
+      /**
+       * set a cache value before the compute function completes.
+       *
+       * useful to handle recursive access.
+       */
+      presolve: (v: V) => void,
+    ) => V | Promise<V>,
+  ) => V | Promise<V>;
+  $value: <T>() => AsyncCache<T>;
+  invalidate: (key: string) => void;
+}
 
-  cached<V1 extends V = V>(key: string, fn: () => V1 | Promise<V1>): V1 | Promise<V1> {
-    let cached = this.store.get(key);
-    if (cached !== undefined) return cached as V1;
+/**
+ * cache for async resources, finished promises will be resolved into original value, otherwise wrapped with a promise.
+ */
+export function createCache<V>(store = new Map<string, V | Promise<V>>()): AsyncCache<V> {
+  return {
+    cached(key, fn) {
+      let cached = store.get(key);
+      if (cached) return cached;
 
-    cached = fn();
-    this.store.set(key, cached);
-    return cached as V1;
-  }
+      cached = fn((v) => store.set(key, v));
+      if (cached instanceof Promise) {
+        cached = cached.then((out) => {
+          // replace with resolved if still exists
+          if (store.has(key)) {
+            store.set(key, out);
+          }
+
+          return out;
+        });
+      }
+      store.set(key, cached);
+      return cached;
+    },
+    invalidate(key) {
+      store.delete(key);
+    },
+    $value<T>() {
+      return this as unknown as AsyncCache<T>;
+    },
+  };
 }
