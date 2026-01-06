547d728c8f238aee7f69579b981dc0f1eb9feea7
Fuma Nama <god63820869@gmail.com>
2026-01-06 19:40:06 +0800

    MDX: fix Vite types

--- packages/mdx/src/loaders/mdx/index.ts
+++ packages/mdx/src/loaders/mdx/index.ts
@@ -1,5 +1,4 @@
 import { fumaMatter } from '@/utils/fuma-matter';
-import type { SourceMap } from 'rollup';
 import type { Loader } from '@/loaders/adapter';
 import { z } from 'zod';
 import type { DocCollectionItem } from '@/config/build';
@@ -99,7 +98,7 @@ export function createMdxLoader({ getCore }: ConfigLoader): Loader {
 
       const out = {
         code: String(compiled.value),
-        map: compiled.map as SourceMap,
+        map: compiled.map,
       };
 
       await after?.();
