732b4d4285577230e3c9f73ed509e8a6fe888510
Fuma Nama <god63820869@gmail.com>
2026-01-09 13:47:43 +0800

    Chore: bump deps & fix peer deps

--- packages/create-app/template/+next+fuma-docs-mdx/next.config.mjs
+++ packages/create-app/template/+next+fuma-docs-mdx/next.config.mjs
@@ -5,6 +5,14 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/docs/:path*.mdx',
+        destination: '/llms.mdx/docs/:path*',
+      },
+    ];
+  },
 };
 
 export default withMDX(config);
