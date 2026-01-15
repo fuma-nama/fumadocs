219db364389d919559bb53871f691d36f5ad65d0
Fuma Nama <god63820869@gmail.com>
2026-01-15 20:59:51 +0800

    make headless only

--- packages/core/src/source/index.ts
+++ packages/core/src/source/index.ts
@@ -1,7 +1,6 @@
 export * from './source';
 export * from './loader';
 export { getSlugs } from './plugins/slugs';
-export { statusBadgesPlugin, type StatusBadgeItem } from './plugins/status-badges';
 export { FileSystem } from './storage/file-system';
 export * as PathUtils from './path';
 
