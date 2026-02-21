e9ec606ce9c4dfd5a3a629004aff913de93b67a7
Kane Wang <kane@yeecord.com>
2026-02-22 03:55:59 +0800

    fix webp suffix

--- examples/react-router/app/lib/og.ts
+++ examples/react-router/app/lib/og.ts
@@ -1,5 +1,5 @@
 export function getPageImagePath(slugs: string[]) {
-  const segments = [...slugs, 'image.png'];
+  const segments = [...slugs, 'image.webp'];
 
   return `/og/docs/${segments.join('/')}`;
 }
