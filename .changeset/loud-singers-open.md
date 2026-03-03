4d05c4e09ca8c2f6a23ee2f2e3c034a5ca177c5b
Fuma Nama <god63820869@gmail.com>
2026-03-03 19:53:25 +0800

    Core: Generate breadcrumbs for custom `buildIndex` option.

---
+++ .changeset/loud-singers-open.md
@@ -0,0 +1,5 @@
+---
+'fumadocs-core': patch
+---
+
+[Search API] Generate breadcrumbs for custom `buildIndex` option.
--- packages/core/src/search/orama/create-from-source.ts
+++ packages/core/src/search/orama/create-from-source.ts
@@ -13,55 +13,58 @@ import type { Language } from '@orama/orama';
 import { findPath } from '@/page-tree/utils';
 import type { Awaitable } from '@/types';
 
-function defaultBuildIndex<C extends LoaderConfig>(source: LoaderOutput<C>) {
-  function isBreadcrumbItem(item: unknown): item is string {
-    return typeof item === 'string' && item.length > 0;
+async function buildIndexDefault(page: Page): Promise<AdvancedIndex> {
+  let structuredData: StructuredData | undefined;
+
+  if ('structuredData' in page.data) {
+    structuredData =
+      typeof page.data.structuredData === 'function'
+        ? ((await page.data.structuredData()) as StructuredData)
+        : (page.data.structuredData as StructuredData);
+  } else if ('load' in page.data && typeof page.data.load === 'function') {
+    structuredData = (await page.data.load()).structuredData;
   }
 
-  return async (page: Page): Promise<AdvancedIndex> => {
-    let breadcrumbs: string[] | undefined;
-    let structuredData: StructuredData | undefined;
-
-    if ('structuredData' in page.data) {
-      structuredData = page.data.structuredData as StructuredData;
-    } else if ('load' in page.data && typeof page.data.load === 'function') {
-      structuredData = (await page.data.load()).structuredData;
-    }
-
-    if (!structuredData)
-      throw new Error(
-        'Cannot find structured data from page, please define the page to index function.',
-      );
-
-    const pageTree = source.getPageTree(page.locale);
-    const path = findPath(
-      pageTree.children,
-      (node) => node.type === 'page' && node.url === page.url,
+  if (!structuredData)
+    throw new Error(
+      'Cannot find structured data from page, please define the page to index function.',
     );
-    if (path) {
-      breadcrumbs = [];
-      path.pop();
 
-      if (isBreadcrumbItem(pageTree.name)) {
-        breadcrumbs.push(pageTree.name);
-      }
+  return {
+    title: page.data.title ?? basename(page.path, extname(page.path)),
+    description: page.data.description,
+    url: page.url,
+    id: page.url,
+    structuredData,
+  };
+}
 
-      for (const segment of path) {
-        if (!isBreadcrumbItem(segment.name)) continue;
+function isBreadcrumbItem(item: unknown): item is string {
+  return typeof item === 'string' && item.length > 0;
+}
 
-        breadcrumbs.push(segment.name);
-      }
+export function buildBreadcrumbsDefault<C extends LoaderConfig>(
+  source: LoaderOutput<C>,
+  page: Page,
+): string[] | undefined {
+  const pageTree = source.getPageTree(page.locale);
+  const path = findPath(pageTree.children, (node) => node.type === 'page' && node.url === page.url);
+
+  if (path) {
+    const breadcrumbs: string[] = [];
+    path.pop();
+
+    if (isBreadcrumbItem(pageTree.name)) {
+      breadcrumbs.push(pageTree.name);
     }
 
-    return {
-      title: page.data.title ?? basename(page.path, extname(page.path)),
-      breadcrumbs,
-      description: page.data.description,
-      url: page.url,
-      id: page.url,
-      structuredData,
-    };
-  };
+    for (const segment of path) {
+      if (!isBreadcrumbItem(segment.name)) continue;
+      breadcrumbs.push(segment.name);
+    }
+
+    return breadcrumbs;
+  }
 }
 
 interface Options<C extends LoaderConfig> extends Omit<AdvancedOptions, 'indexes'> {
@@ -82,18 +85,23 @@ export function createFromSource<C extends LoaderConfig>(
   source: LoaderOutput<C>,
   options: Options<C> = {},
 ): SearchAPI {
-  const { buildIndex = defaultBuildIndex(source) } = options;
+  const { buildIndex = buildIndexDefault } = options;
 
   if (source._i18n) {
     return createI18nSearchAPI('advanced', {
       ...options,
       i18n: source._i18n,
-      indexes: async () => {
+      async indexes() {
         const indexes = source.getLanguages().flatMap((entry) => {
-          return entry.pages.map(async (page) => ({
-            ...(await buildIndex(page)),
-            locale: entry.language,
-          }));
+          return entry.pages.map(async (page) => {
+            const index = await buildIndex(page);
+            index.breadcrumbs ??= buildBreadcrumbsDefault(source, page);
+
+            return {
+              ...index,
+              locale: entry.language,
+            };
+          });
         });
 
         return Promise.all(indexes);
@@ -103,8 +111,13 @@ export function createFromSource<C extends LoaderConfig>(
 
   return createSearchAPI('advanced', {
     ...options,
-    indexes: async () => {
-      const indexes = source.getPages().map((page) => buildIndex(page));
+    async indexes() {
+      const indexes = source.getPages().map(async (page) => {
+        const index = await buildIndex(page);
+        index.breadcrumbs ??= buildBreadcrumbsDefault(source, page);
+
+        return index;
+      });
 
       return Promise.all(indexes);
     },
