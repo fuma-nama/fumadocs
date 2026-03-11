53575600a6f91e853e7acf7d9bfcea5433f4d506
Fuma Nama <god63820869@gmail.com>
2026-03-11 20:19:38 +0800

    Chore: update test snapshots

--- packages/core/test/highlight.test.ts
+++ packages/core/test/highlight.test.ts
@@ -5,16 +5,7 @@ test('highlight search results', () => {
   const highlighter = createContentHighlighter('hello world helloworld');
 
   expect(highlighter.highlightMarkdown('oops hello, world hello! worldhello'))
-    .toMatchInlineSnapshot(`
-      "oops <mark>hello</mark>, <mark>world</mark> <mark>hello</mark>! <mark>world</mark><mark>hello</mark>
-      "
-    `);
-  expect(highlighter.highlightMarkdown('helloworld!!!')).toMatchInlineSnapshot(`
-    "<mark>hello</mark><mark>world</mark>!!!
-    "
-  `);
-  expect(highlighter.highlightMarkdown('wor ld hello')).toMatchInlineSnapshot(`
-    "wor ld <mark>hello</mark>
-    "
-  `);
+    .toMatchInlineSnapshot(`"oops <mark>hello</mark>, <mark>world</mark> <mark>hello</mark>! <mark>world</mark><mark>hello</mark>"`);
+  expect(highlighter.highlightMarkdown('helloworld!!!')).toMatchInlineSnapshot(`"<mark>hello</mark><mark>world</mark>!!!"`);
+  expect(highlighter.highlightMarkdown('wor ld hello')).toMatchInlineSnapshot(`"wor ld <mark>hello</mark>"`);
 });
