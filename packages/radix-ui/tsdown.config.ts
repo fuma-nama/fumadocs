86d3abb8c0c0434ae69383a8671a9d52ce0afc03
Fuma Nama <god63820869@gmail.com>
2026-03-11 17:07:54 +0800

    UI: fix broken tsdown CSS logic

--- packages/radix-ui/tsdown.config.ts
+++ packages/radix-ui/tsdown.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'tsdown';
-import fs from 'node:fs/promises';
 import { compileInline } from './scripts/compile-inline.utils';
 
 export default defineConfig({
@@ -17,11 +16,6 @@ export default defineConfig({
   },
   async onSuccess() {
     await compileInline();
-    // wait until https://github.com/rolldown/tsdown/issues/472
-    let content = (await fs.readFile('dist/components/image-zoom.js')).toString();
-    content = content.replaceAll(`import "./image-zoom2.js";`, `import "./image-zoom.css";`);
-    await fs.writeFile('dist/components/image-zoom.js', content);
-    console.log('CSS import updated');
   },
   deps: {
     onlyAllowBundle: [],
