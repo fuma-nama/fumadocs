8b3179f91ddfd53cebbc0e41f68a2943312fdd44
Fuma Nama <god63820869@gmail.com>
2026-03-13 16:22:55 +0800

    Content: output as collection pack

---
+++ packages/content/src/runtime.ts
@@ -0,0 +1,56 @@
+import type { FileCollectionStore } from 'fuma-content/collections/runtime/file-store';
+import type { MDXStoreLazyData, MDXStoreData } from 'fuma-content/collections/mdx/runtime';
+import type { MetaData, PageData, Source } from 'fumadocs-core/source';
+
+type ToPageData<T> =
+  T extends MDXStoreData<infer Frontmatter>
+    ? Frontmatter & T
+    : T extends MDXStoreLazyData<infer Frontmatter, unknown>
+      ? Frontmatter & T
+      : never;
+
+export function toFumadocsSource<
+  Mdx extends MDXStoreData<PageData> | MDXStoreLazyData<PageData, unknown> = MDXStoreData<
+    PageData,
+    unknown
+  >,
+  Meta extends { data: MetaData } = { data: MetaData },
+>(mdxStore?: FileCollectionStore<Mdx>, metaStore?: FileCollectionStore<Meta>) {
+  const out: Source<{
+    pageData: ToPageData<Mdx>;
+    metaData: Meta['data'];
+  }> = { files: [] };
+  for (const page of mdxStore?.list() ?? []) {
+    out.files.push({
+      type: 'page',
+      data: ('compiled' in page
+        ? { ...page, ...page.compiled.frontmatter }
+        : { ...page, ...page.frontmatter }) as unknown as ToPageData<Mdx>,
+      path: page.path,
+      absolutePath: page.fullPath,
+    });
+  }
+  for (const file of metaStore?.list() ?? []) {
+    out.files.push({
+      type: 'meta',
+      data: file.data,
+      path: file.path,
+      absolutePath: file.fullPath,
+    });
+  }
+  return out;
+}
+
+export function docsStore<
+  Mdx extends MDXStoreData<PageData> | MDXStoreLazyData<PageData, unknown> = MDXStoreData<
+    PageData,
+    unknown
+  >,
+  Meta extends { data: MetaData } = { data: MetaData },
+>(mdxStore: FileCollectionStore<Mdx>, metaStore: FileCollectionStore<Meta>) {
+  return {
+    toFumadocsSource() {
+      return toFumadocsSource(mdxStore, metaStore);
+    },
+  };
+}
--- packages/content/tsdown.config.ts
+++ packages/content/tsdown.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsdown';
 
 export default defineConfig({
-  entry: ['./src/index.ts'],
+  entry: ['./src/{index,runtime}.ts'],
   format: 'esm',
   dts: true,
   fixedExtension: false,
