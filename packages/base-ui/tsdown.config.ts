b8eeca14d3fd69dbbfddc473918786caa6933633
Fuma Nama <god63820869@gmail.com>
2026-05-07 22:40:41 +0800

    Bump deps

--- packages/base-ui/tsdown.config.ts
+++ packages/base-ui/tsdown.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'tsdown';
-import { compileInline } from './scripts/compile-inline.utils';
+import { Scanner } from '@tailwindcss/oxide';
+import { mkdir, writeFile } from 'node:fs/promises';
+import path from 'node:path';
 
 export default defineConfig({
   format: 'esm',
@@ -41,3 +43,55 @@ export default defineConfig({
     },
   },
 });
+
+export async function compileInline() {
+  await mkdir('css/generated', { recursive: true });
+  const scanner = new Scanner({
+    sources: [
+      {
+        base: path.resolve('src'),
+        pattern: 'components/**/*.{ts,tsx}',
+        negated: false,
+      },
+      {
+        base: path.resolve('src'),
+        pattern: 'layouts/shared/**/*.{ts,tsx}',
+        negated: false,
+      },
+      {
+        base: path.resolve('src'),
+        pattern: '*.{ts,tsx}',
+        negated: false,
+      },
+    ],
+  });
+
+  const commonNames = scanner.scan();
+  await writeFile('css/generated/shared.css', namesToFile(commonNames));
+
+  const layouts = ['flux', 'notebook', 'home', 'docs'];
+  const commonNameSet = new Set(commonNames);
+
+  for (const layout of layouts) {
+    const scanner = new Scanner({
+      sources: [
+        {
+          base: path.resolve('src'),
+          pattern: `layouts/${layout}/**/*.{ts,tsx}`,
+          negated: false,
+        },
+      ],
+    });
+
+    await writeFile(
+      `css/generated/${layout}.css`,
+      namesToFile(scanner.scan().filter((name) => !commonNameSet.has(name))),
+    );
+  }
+
+  console.log('generated CSS files');
+}
+
+function namesToFile(names: string[]) {
+  return names.map((name) => `@source inline(${JSON.stringify(name)});`).join('\n');
+}
