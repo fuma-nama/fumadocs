29b65def829ef1d486d8e10ea58b36a10a4280a1
Fuma Nama <god63820869@gmail.com>
2026-05-11 20:38:28 +0800

    Refactor: Move Fumapress to another repository

--- packages/press-ai/CHANGELOG.md
+++
@@ -1,8 +0,0 @@
-# @fumapress/ai
-
-## 0.2.1
-
-### Patch Changes
-
-- Updated dependencies [3b3083e]
-  - fumapress@0.2.1
--- packages/press-core/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './config';
-export type { AppContext, AppContextData } from './lib/shared';
-export type { ServerPlugin, Adapter } from '@/lib/types';
