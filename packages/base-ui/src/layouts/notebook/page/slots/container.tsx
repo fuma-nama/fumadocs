e616cb30f1ced73faff86b9bdfe14981bcadab69
Fuma Nama <god63820869@gmail.com>
2026-08-27 21:53:14 +0800

    fix(ui): improve solar theme on new `<main>` container

--- packages/base-ui/src/layouts/notebook/page/slots/container.tsx
+++ packages/base-ui/src/layouts/notebook/page/slots/container.tsx
@@ -7,13 +7,14 @@ export function Container(props: ComponentProps<'article'>) {
   const { full } = useDocsPage();
 
   return (
-    <main className="grid [grid-area:main]">
+    <main className="grid [grid-area:main]" data-layout-main="">
       <article
         id="nd-page"
+        data-layout-content=""
         data-full={full}
         {...props}
         className={cn(
-          'flex flex-col px-4 py-6 gap-4 md:px-6 md:pt-8 xl:px-8 xl:pt-14 *:max-w-[900px]',
+          'flex flex-col min-w-0 px-4 py-6 gap-4 md:px-6 md:pt-8 xl:px-8 xl:pt-14 *:max-w-[900px]',
           full && '*:max-w-[1285px]',
           props.className,
         )}
--- packages/radix-ui/src/layouts/flux/page/slots/container.tsx
+++ packages/radix-ui/src/layouts/flux/page/slots/container.tsx
@@ -8,13 +8,14 @@ export function Container(props: ComponentProps<'article'>) {
   const { full } = useDocsPage();
 
   return (
-    <main className="grid [grid-area:main] justify-items-center">
+    <main className="grid [grid-area:main] justify-items-center" data-layout-main="">
       <article
         id="nd-page"
+        data-layout-content=""
         data-full={full}
         {...props}
         className={cn(
-          'flex flex-col w-full max-w-[900px] px-4 py-6 gap-4 md:px-6 md:pt-8 xl:px-8 xl:pt-14',
+          'flex flex-col min-w-0 w-full max-w-[900px] px-4 py-6 gap-4 md:px-6 md:pt-8 xl:px-8 xl:pt-14',
           full && 'max-w-[1200px]',
           props.className,
         )}
