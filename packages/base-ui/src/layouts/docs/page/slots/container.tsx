dbf0f1282cc12b6bb2966b73147486fc271596de
Fuma Nama <god63820869@gmail.com>
2026-08-27 19:57:24 +0800

    fix(ui): Next.js auto-scroll bug

--- packages/base-ui/src/layouts/docs/page/slots/container.tsx
+++ packages/base-ui/src/layouts/docs/page/slots/container.tsx
@@ -8,13 +8,13 @@ export function Container(props: ComponentProps<'article'>) {
   const { full } = useDocsPage();
 
   return (
-    <main className="contents">
+    <main className="grid [grid-area:main] justify-items-center">
       <article
         id="nd-page"
         data-full={full}
         {...props}
         className={cn(
-          'flex flex-col w-full max-w-[900px] mx-auto [grid-area:main] px-4 py-6 gap-4 md:px-6 md:pt-8 xl:px-8 xl:pt-14',
+          'flex flex-col w-full max-w-[900px] px-4 py-6 gap-4 md:px-6 md:pt-8 xl:px-8 xl:pt-14',
           full && 'max-w-[1168px]',
           props.className,
         )}
