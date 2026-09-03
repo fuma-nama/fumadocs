337b23564662da3b3691ecad76e38c3d0ad7d97b
Fuma Nama <god63820869@gmail.com>
2026-09-03 17:40:09 +0800

    chore: bump typescript

---
+++ packages/shared/registry.d.ts
@@ -0,0 +1,4 @@
+import type { Component } from 'fuma-cli/compiler';
+export declare function findSlotComponents(dir: string): Promise<Component[]>;
+export declare const commonComponents: Component[];
+//# sourceMappingURL=registry.d.ts.map
